6505f307328d194646bc309b25fc66a7967b1511
egor dorosenko <dorohenkojord@gmail.com>
2022-06-19 19:02:41 +0300

    add url

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSort } from '../redux/slices/filterSlice';
 
-const list = [
+export const list = [
   { name: 'популярность(DESC)', sort: 'rating' },
   { name: 'популярность(ASC)', sort: '-rating' },
   { name: 'цене(DESC)', sort: 'price' },
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
+import qs from 'qs';
+import { useNavigate } from 'react-router-dom';
 
-import { setCategotyId, setCurrentPage } from '../redux/slices/filterSlice';
+import {
+  setCategotyId,
+  setCurrentPage,
+  setFilters,
+} from '../redux/slices/filterSlice';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Pagination from '../components/Pagination/index';
 import { SearchContext } from '../App';
+import { list } from '../components/Sort';
 
 const Home = () => {
   const { searchValue } = React.useContext(SearchContext);
@@ -16,6 +23,10 @@ const Home = () => {
   const [isLoading, setLoading] = React.useState(true);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isSearch = React.useRef(false);
+  const isMounted = React.useRef(false);
+
   const categoryId = useSelector((state) => state.filterReducer.categoryId);
   const sortType = useSelector((state) => state.filterReducer.sort.sort);
   const currentPage = useSelector((state) => state.filterReducer.currentPage);
@@ -27,7 +38,7 @@ const Home = () => {
   const onChangePage = (number) => {
     dispatch(setCurrentPage(number));
   };
-  React.useEffect(() => {
+  const fetchPizzas = () => {
     setLoading(true);
     const sortBy = sortType.replace('-', '');
     const order = sortType.includes('-') ? 'asc' : 'desc';
@@ -42,8 +53,43 @@ const Home = () => {
         setFetchItems(res.data);
         setLoading(false);
       });
+  };
+
+  React.useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+      const sort = list.find((obj) => obj.sort === params.sortType);
+
+      dispatch(
+        setFilters({
+          ...params,
+          sort,
+        })
+      );
+      isSearch.current = true;
+    }
+  }, [dispatch]);
+
+  React.useEffect(() => {
+    if (!isSearch.current) {
+      fetchPizzas();
+    }
     window.scrollTo(0, 0);
+    isSearch.current = false;
   }, [categoryId, sortType, searchValue, currentPage]);
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortType,
+        categoryId,
+        currentPage,
+      });
+
+      navigate(`?${queryString}`);
+    }
+    isMounted.current = true;
+  }, [categoryId, sortType, searchValue, currentPage, navigate]);
 
   return (
     <div className="container">
